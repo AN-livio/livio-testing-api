6dc6cd0daf627a7c6844fbc83f2914f284503909
AN-livio <anirudh@golivio.com>
2021-09-23 09:17:53 +0530

    added cors

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 let express = require("express");
+let cors = require("cors");
 let userRouter = require("./routes/user");
 let adminRouter = require("./routes/admin");
 
 require("./utils/db");
 let app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use("/screening", userRouter);
 app.use("/admin", adminRouter);
 
-let port = process.env.PORT || 5000
+let port = process.env.PORT || 5000;
 app.listen(port);
